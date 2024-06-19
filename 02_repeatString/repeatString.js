375513fa443dccedc3782351145e4ff85f3e2473
wistromh04 <wherfordt@gmail.com>
2024-06-19 18:33:30 -0500

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+   let newStr = "";
+    for (let i = 0; i < n; i++) {
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
