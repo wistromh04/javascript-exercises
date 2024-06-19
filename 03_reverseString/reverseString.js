7dbf77f861d8c25cb2d640f9b1f42508407a9198
wistromh04 <wherfordt@gmail.com>
2024-06-19 18:40:24 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let reversed = "";
+for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+}
+return reversed;
 };
-
+reverseString("hello")
 // Do not edit below this line
 module.exports = reverseString;
