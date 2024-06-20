1cbaaaee5a6a5c728fcb9e8c3aee8bcea5c0e009
wistromh04 <wherfordt@gmail.com>
2024-06-19 22:18:59 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
