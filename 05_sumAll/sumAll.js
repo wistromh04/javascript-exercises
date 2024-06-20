94e7521100fa4ebd078f6cff1d3299635d06b59e
wistromh04 <wherfordt@gmail.com>
2024-06-19 22:25:39 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+let sum = 0;
+const start = Math.min(num1, num2);
+const end = Math.max(num1, num2);
+for (let i = start; i <= end; i++) {
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+sumAll(1, 4)
